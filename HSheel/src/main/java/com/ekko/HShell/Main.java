53bed71a26d4fc4068d0a9643f8f06ad8a4c3b19
EKKO <189890049@qq.com>
2023-06-08 23:23:29 +0800

    more

--- HSheel/src/main/java/com/ekko/HShell/Main.java
+++ HSheel/src/main/java/com/ekko/HShell/Main.java
@@ -4,9 +4,6 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.*;
 
 import java.io.IOException;
-import java.net.URI;
-import java.util.Arrays;
-
 /**
  * @Author YLL
  * @Date 2023/6/8 20:20
@@ -72,8 +69,11 @@ public class Main {
             fs.close();
         }
     }
-
+//1.使用HShell -cp 本地路径 HDFS路径，将文件从Linux本地文件系统拷贝到HDFS指定路径上。
     private static void copyToLocalFileSystem(String source, String destination, FileSystem fs) throws IOException {
+        /**
+         * 传入参数,调用copyFromLocalFile api
+         */
         Path sourcePath = new Path(source);
         Path destinationPath = new Path(destination);
 
@@ -81,7 +81,7 @@ public class Main {
 
         System.out.println("File copied successfully.");
     }
-
+// 2.使用HShell -rm 路径删除文件
     private static void deleteFile(String path, FileSystem fs) throws IOException {
         Path filePath = new Path(path);
 
@@ -93,7 +93,7 @@ public class Main {
             System.out.println("Failed to delete file or file does not exist.");
         }
     }
-
+//3.使用HShell -rm -r 路径删除目录
     private static void deleteDirectory(String path, FileSystem fs) throws IOException {
         Path directoryPath = new Path(path);
 
@@ -105,7 +105,7 @@ public class Main {
             System.out.println("Failed to delete directory or directory does not exist.");
         }
     }
-
+//4.使用HShell -cp -r 本地目录路径 HDFS路径，将目录从Linux本地拷贝到HDFS指定路径上。
     private static void listStatus(String path, FileSystem fs) throws IOException {
         Path directoryPath = new Path(path);
 
@@ -120,7 +120,7 @@ public class Main {
             System.out.println("Directory does not exist: " + directoryPath);
         }
     }
-
+//5.使用HShell -list 路径显示某个文件的信息或者某个目录的信息
     private static void moveFile(String source, String destination, FileSystem fs) throws IOException {
         Path sourcePath = new Path(source);
         Path destinationPath = new Path(destination);
@@ -133,6 +133,8 @@ public class Main {
             System.out.println("Failed to move file or file does not exist.");
         }
     }
+//    6.使用HShell -mv 路径 路径移动文件或者重命名文件
+
     private static void findFile(String fileName, String directory, FileSystem fs) throws IOException {
         Path directoryPath = new Path(directory);
 
@@ -142,7 +144,7 @@ public class Main {
             System.out.println("Directory does not exist: " + directoryPath);
         }
     }
-
+//7.使用HShell -find 文件名 目录实现在目录下递归查找某个文件名的文件。
     private static void searchFiles(String fileName, Path path, FileSystem fs) throws IOException {
         for (org.apache.hadoop.fs.FileStatus fileStatus : fs.listStatus(path)) {
             if (fileStatus.isDirectory()) {
