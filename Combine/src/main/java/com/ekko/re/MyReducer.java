9c777b28f7aa6d5f902b7542d168fe27eda53ff8
EKKO <189890049@qq.com>
2023-06-08 22:07:54 +0800

    Combine Demo

--- Combine/src/main/java/com/ekko/re/MyReducer.java
+++ Combine/src/main/java/com/ekko/re/MyReducer.java
@@ -1,19 +1,20 @@
 package com.ekko.re;
 
 import java.io.IOException;
+
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class MyReducer extends Reducer<Text,Text,Text,Text>{
+public class MyReducer extends Reducer<Text, Text, Text, Text> {
     private Text result = new Text();
-    public void reduce(Text key,Iterable<Text>values,Context context) throws 
 
-IOException, InterruptedException{
-        String value =new String();
-        for(Text value1:values){
-            value += value1.toString()+" ; ";
+    public void reduce(Text key, Iterable<Text> values, Context context) throws
+            IOException, InterruptedException {
+        String value = new String();
+        for (Text value1 : values) {
+            value += value1.toString() + " ; ";
         }
         result.set(value);
-        context.write(key,result);
+        context.write(key, result);
     }
 }
