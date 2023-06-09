22dc40b769e6e754ad9160c95b89a8e03a7cb7e9
EKKO <189890049@qq.com>
2023-06-09 20:12:55 +0800

    祖孙辈关系

--- Relation/src/main/java/com/ekko/mr/MyMapper.java
+++ Relation/src/main/java/com/ekko/mr/MyMapper.java
@@ -24,7 +24,6 @@ public class MyMapper extends Mapper<LongWritable, Text, Text, Text> {
         throws IOException, InterruptedException {
         //具体业务逻辑就写在这个方法体中，而且我们业务要处理的数据已经被框架传递进来，在方法的参数中key-value
         //key是这一行数据的起始偏移量，value是这一行的文本内容
-        
         //1:切分名字，用空格隔开，前面的是孩子，后面的是父母
         String child = value.toString().split("\\s+")[0];
         String parent = value.toString().split("\\s+")[1];
