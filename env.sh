97b81a1b9ac972a0a02104e5f4e715391a48bbbd
EKKO <189890049@qq.com>
2023-06-10 09:08:13 +0800

    hbase

--- env.sh
+++ env.sh
@@ -9,6 +9,10 @@
 "yes" | cp CumtHadoopLAB/yarn-site.xml /headless/course
 
 "yes" | cp CumtHadoopLAB/slaves /headless/course
+
+"yes" | cp CumtHadoopLAB/hbase/hbaseIni.sh /headless/cours
+"yes" | cp CumtHadoopLAB/hbase/hbase-site.xml /headless/cours
+
 docker run --name master01 --privileged -itd -v /cgsrc:/cgsrc:ro -v /headless/course/:/course hadoop_node /service_start.sh
 
 docker exec -it master01 /bin/bash
