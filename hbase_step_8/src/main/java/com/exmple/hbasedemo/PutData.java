dd79dd0c5650a2445f5fb8e01cab87772b89bf63
EKKO <189890049@qq.com>
2023-06-10 16:36:12 +0800

    Myconnect TableExist CreateTable ListTable DeleteTable PutData ScanData

--- hbase_step_8/src/main/java/com/exmple/hbasedemo/PutData.java
+++ hbase_step_8/src/main/java/com/exmple/hbasedemo/PutData.java
@@ -3,45 +3,50 @@ package com.exmple.hbasedemo;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.*;
 import org.apache.hadoop.hbase.client.*;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class PutData{
+public class PutData {
     public static Configuration configuration;
     public static Connection connection;
     public static Admin admin;
-    public static void main(String[] args)throws IOException{
+
+    public static void main(String[] args) throws IOException {
         init();
         putData("student");
         close();
-    } 
+    }
+
     //建立连接
-    public static void init(){
-    //初始化Configuration 对象
-        configuration  = HBaseConfiguration.create();
-        try{
-        //根据 Configuration对象初始化Connection 对象
+    public static void init() {
+        //初始化Configuration 对象
+        configuration = HBaseConfiguration.create();
+        try {
+            //根据 Configuration对象初始化Connection 对象
             connection = ConnectionFactory.createConnection(configuration);
             //获取 Admin 对象实例
             admin = connection.getAdmin();
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
     //关闭连接
-    public static void close(){
-        try{
-            if(admin != null){
+    public static void close() {
+        try {
+            if (admin != null) {
                 admin.close();
             }
-            if(null != connection){
+            if (null != connection) {
                 connection.close();
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
     public static void putData(String tableName) throws IOException {
         FileReader reader = new FileReader("/data/input.dat");
 
@@ -53,17 +58,17 @@ public class PutData{
         BufferedReader br = new BufferedReader(reader);
         String str = null;
         Table table = connection.getTable(TableName.valueOf(tableName));
-        while((str = br.readLine()) != null) {
-        System.out.println("processing line:" + str);
-        String[] cols = str.split(",");
-        Put put = new Put(cols[0].getBytes());
-        put.addColumn("Name".getBytes(), "".getBytes(), cols[1].getBytes());
-        put.addColumn("Sex".getBytes(), "".getBytes(), cols[2].getBytes());
-        put.addColumn("Age".getBytes(), "".getBytes(), cols[3].getBytes());
-        put.addColumn("Course".getBytes(), "Math".getBytes(), cols[4].getBytes());
-        put.addColumn("Course".getBytes(), "English".getBytes(), cols[5].getBytes());
-        put.addColumn("Course".getBytes(), "Chinese".getBytes(), cols[6].getBytes());
-        table.put(put);
+        while ((str = br.readLine()) != null) {
+            System.out.println("processing line:" + str);
+            String[] cols = str.split(",");
+            Put put = new Put(cols[0].getBytes());
+            put.addColumn("Name".getBytes(), "".getBytes(), cols[1].getBytes());
+            put.addColumn("Sex".getBytes(), "".getBytes(), cols[2].getBytes());
+            put.addColumn("Age".getBytes(), "".getBytes(), cols[3].getBytes());
+            put.addColumn("Course".getBytes(), "Math".getBytes(), cols[4].getBytes());
+            put.addColumn("Course".getBytes(), "English".getBytes(), cols[5].getBytes());
+            put.addColumn("Course".getBytes(), "Chinese".getBytes(), cols[6].getBytes());
+            table.put(put);
         }
         table.close();
         br.close();
--- hbase_step_8/src/main/java/com/exmple/hbasedemo/ScanData.java
+++ hbase_step_8/src/main/java/com/exmple/hbasedemo/ScanData.java
@@ -8,40 +8,45 @@ import org.apache.hadoop.hbase.filter.Filter;
 import org.apache.hadoop.hbase.filter.FilterList;
 import org.apache.hadoop.hbase.filter.SingleColumnValueFilter;
 import org.apache.hadoop.hbase.util.Bytes;
+
 import java.io.IOException;
 import java.util.ArrayList;
-public class ScanData{
+
+public class ScanData {
     public static Configuration configuration;
     public static Connection connection;
     public static Admin admin;
+
     //建立连接
-    public static void init(){
-    //初始化Configuration 对象
-        configuration  = HBaseConfiguration.create();
-        try{
-        //根据 Configuration对象初始化Connection 对象
+    public static void init() {
+        //初始化Configuration 对象
+        configuration = HBaseConfiguration.create();
+        try {
+            //根据 Configuration对象初始化Connection 对象
             connection = ConnectionFactory.createConnection(configuration);
             //获取 Admin 对象实例
             admin = connection.getAdmin();
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
         System.out.println("init success!");
     }
+
     //关闭连接
-    public static void close(){
-        try{
-            if(admin != null){
+    public static void close() {
+        try {
+            if (admin != null) {
                 admin.close();
             }
-            if(null != connection){
+            if (null != connection) {
                 connection.close();
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    public static void main(String[] args)throws IOException{
+
+    public static void main(String[] args) throws IOException {
         init();
         System.out.println("Scan all data...");
         scanAllData("student");
@@ -50,60 +55,63 @@ public class ScanData{
         System.out.println("Scan part data2...");
         scanPartData2("student");
         close();
-    } 
+    }
+
     public static void scanAllData(String tableName) {
-    try {
+        try {
             Table t = connection.getTable(TableName.valueOf(tableName));
             ResultScanner rs = t.getScanner(new Scan());
-            for(Result r: rs) {
-            for(Cell cell: r.rawCells()) {
-            System.out.print(Bytes.toString(CellUtil.cloneRow(cell)));
-            System.out.print("\t"+Bytes.toString(CellUtil.cloneFamily(cell)));
-            System.out.print("\t"+Bytes.toString(CellUtil.cloneQualifier(cell)));
-            System.out.print("\t"+Bytes.toString(CellUtil.cloneValue(cell))+"\n");
-            }
+            for (Result r : rs) {
+                for (Cell cell : r.rawCells()) {
+                    System.out.print(Bytes.toString(CellUtil.cloneRow(cell)));
+                    System.out.print("\t" + Bytes.toString(CellUtil.cloneFamily(cell)));
+                    System.out.print("\t" + Bytes.toString(CellUtil.cloneQualifier(cell)));
+                    System.out.print("\t" + Bytes.toString(CellUtil.cloneValue(cell)) + "\n");
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
     public static void scanPartData1(String tableName) {
-    try {
+        try {
             Table t = connection.getTable(TableName.valueOf(tableName));
             Filter filter = new SingleColumnValueFilter("Course".getBytes(), "Math".getBytes(), CompareOp.GREATER, "90".getBytes());
             Scan scan = new Scan();
             scan.setFilter(filter);
             ResultScanner rs = t.getScanner(scan);
-            for(Result r: rs) {
-            for(Cell cell: r.rawCells()) {
-            System.out.print(Bytes.toString(CellUtil.cloneRow(cell)));
-            System.out.print("\t"+Bytes.toString(CellUtil.cloneFamily(cell)));
-            System.out.print("\t"+Bytes.toString(CellUtil.cloneQualifier(cell)));
-            System.out.print("\t"+Bytes.toString(CellUtil.cloneValue(cell))+"\n");
-            }
+            for (Result r : rs) {
+                for (Cell cell : r.rawCells()) {
+                    System.out.print(Bytes.toString(CellUtil.cloneRow(cell)));
+                    System.out.print("\t" + Bytes.toString(CellUtil.cloneFamily(cell)));
+                    System.out.print("\t" + Bytes.toString(CellUtil.cloneQualifier(cell)));
+                    System.out.print("\t" + Bytes.toString(CellUtil.cloneValue(cell)) + "\n");
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
     public static void scanPartData2(String tableName) {
         try {
             Table t = connection.getTable(TableName.valueOf(tableName));
-            Filter filter1 = new SingleColumnValueFilter("Course".getBytes(), "Math".getBytes(), CompareOp.GREATER,"85".getBytes());
+            Filter filter1 = new SingleColumnValueFilter("Course".getBytes(), "Math".getBytes(), CompareOp.GREATER, "85".getBytes());
             Filter filter2 = new SingleColumnValueFilter("Course".getBytes(), "Chinese".getBytes(), CompareOp.LESS, "85".getBytes());
-            ArrayList<Filter> listForFilters = new ArrayList<Filter>(); 
+            ArrayList<Filter> listForFilters = new ArrayList<Filter>();
             listForFilters.add(filter1);
             listForFilters.add(filter2);
-            Filter filterList = new FilterList(FilterList.Operator.MUST_PASS_ALL,listForFilters); 
+            Filter filterList = new FilterList(FilterList.Operator.MUST_PASS_ALL, listForFilters);
             Scan scan = new Scan();
             scan.setFilter(filterList);
             ResultScanner rs = t.getScanner(scan);
-            for(Result r: rs) {
-                for(Cell cell: r.rawCells()) {
+            for (Result r : rs) {
+                for (Cell cell : r.rawCells()) {
                     System.out.print(Bytes.toString(CellUtil.cloneRow(cell)));
-                    System.out.print("\t"+Bytes.toString(CellUtil.cloneFamily(cell)));
-                    System.out.print("\t"+Bytes.toString(CellUtil.cloneQualifier(cell)));
-                    System.out.print("\t"+Bytes.toString(CellUtil.cloneValue(cell))+"\n");
+                    System.out.print("\t" + Bytes.toString(CellUtil.cloneFamily(cell)));
+                    System.out.print("\t" + Bytes.toString(CellUtil.cloneQualifier(cell)));
+                    System.out.print("\t" + Bytes.toString(CellUtil.cloneValue(cell)) + "\n");
                 }
             }
         } catch (IOException e) {
